bf9461ff0c495e4d4db8f4196b5bacb9224cf1bb
Peng Ma <peng.ma@thoughtworks.com>
2025-05-06 17:47:46 +0800

    [N/A][feat]: Adding a string arrangement includes a check function

--- src/main/java/org/example/string/Solution.java
+++ src/main/java/org/example/string/Solution.java
@@ -41,4 +41,41 @@ public class Solution {
         }
         return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
+
+    public boolean checkInclusion(String t, String s) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> windows = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        int left = 0;
+        int right = 0;
+        int valid = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (need.containsKey(c)) {
+                windows.put(c, windows.getOrDefault(c, 0) + 1);
+                if (windows.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            while ((right - left) >= t.length()) {
+                if (valid == need.size()) {
+                    return true;
+                }
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (windows.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    windows.put(d, windows.get(d) - 1);
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/test/groovy/org/example/string/SolutionTest.groovy
+++ src/test/groovy/org/example/string/SolutionTest.groovy
@@ -19,4 +19,22 @@ class SolutionTest extends Specification {
             "abcdef"         | "xyz"  | ""
             "aabdec"         | "abc"  | "abdec"
     }
+
+    def "Test checkInclusion"() {
+        expect:
+            solution.checkInclusion(t, s) == expected
+
+        where:
+            s                   | t          | expected
+            "abcd"              | "abcd"     | true
+            "abcd"              | "acbd"     | true
+            "abcd"              | "abce"     | false
+            "eidbaooo"          | "ab"       | true
+            "eidboaoo"          | "ab"       | false
+            "abcde"             | "cde"      | true
+            "a"                 | "a"        | true
+            "a"                 | "aa"       | false
+            ""                  | "a"        | false
+            "adcada"            | "abc"      | false
+    }
 }
