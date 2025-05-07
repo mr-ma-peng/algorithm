6e3a80b961fffa228856eaf5d800b79c8cd3d7e5
Peng Ma <peng.ma@thoughtworks.com>
2025-05-07 11:32:07 +0800

    [N/A][feat]: Add unit tests for the BinarySearch class

--- src/main/java/org/example/search/BinarySearch.java
+++ src/main/java/org/example/search/BinarySearch.java
@@ -1,13 +1,9 @@
 package org.example.search;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class BinarySearch {
 
-    private static final Logger LOGGER = Logger.getLogger(BinarySearch.class.getName());
-
-    public static int binarySearch(int[] array, int target) {
+    public int binarySearch(int[] array, int target) {
         int left = 0;
         int right = array.length - 1;
         while (left <= right) {
@@ -21,11 +17,5 @@ public class BinarySearch {
             }
         }
         return -1;
-    }
-
-    public static void main(String[] args) {
-        int[] array = new int[] {1, 3, 4, 5, 7, 11, 15, 17, 22, 45};
-        int target = 7;
-        LOGGER.log(Level.INFO, "Index of target: {0}", binarySearch(array, target));
     }
 }
---
+++ src/test/groovy/org/example/search/BinarySearchTest.groovy
@@ -0,0 +1,24 @@
+package org.example.search
+
+import spock.lang.Specification
+
+class BinarySearchTest extends Specification {
+
+    private BinarySearch binarySearch = new BinarySearch()
+
+    def "Test BinarySearch"() {
+        expect:
+            binarySearch.binarySearch(array as int[], target) == expected
+
+        where:
+            array                               | target | expected
+            [1, 3, 4, 5, 7, 11, 15, 17, 22, 45] | 7      | 4
+            [1, 3, 4, 5, 7, 11, 15, 17, 22, 45] | 1      | 0
+            [1, 3, 4, 5, 7, 11, 15, 17, 22, 45] | 45     | 9
+            [1, 3, 4, 5, 7, 11, 15, 17, 22, 45] | 2      | -1
+            [1, 3, 4, 5, 7, 11, 15, 17, 22, 45] | 100    | -1
+            []                                  | 5      | -1
+            [10]                                | 10     | 0
+            [10]                                | 5      | -1
+    }
+}
