bb31343042441068bc15bf122579214159525c82
Peng Ma <peng.ma@thoughtworks.com>
2025-05-13 08:18:14 +0800

    [N/A][feat]: Added array color sorting

--- src/main/java/org/example/array/Solution.java
+++ src/main/java/org/example/array/Solution.java
@@ -316,4 +316,28 @@ public class Solution {
         }
         return matrix;
     }
+
+    public void sortColors(int[] nums) {
+        int p0 = 0;
+        int p2 = nums.length - 1;
+        int p = 0;
+        while (p <= p2) {
+            if (nums[p] == 0) {
+                swap(nums, p, p0++);
+            } else if (nums[p] == 2) {
+                swap(nums, p, p2--);
+            } else if (nums[p] == 1) {
+                p++;
+            }
+            if (p < p0) {
+                p = p0;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
